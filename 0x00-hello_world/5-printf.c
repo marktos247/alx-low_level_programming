54dbaeb85071f9b1b97a8cef532aaf961127e72d
marktos247 <aj.marktos@yahoo.com>
2023-06-17 02:52:10 -0700

    print_sizes

--- 0x00-hello_world/5-printf.c
+++ 0x00-hello_world/5-printf.c
@@ -3,8 +3,9 @@
  * main - Print a line using printf function
  * Return: 0
  */
+
 int main(void)
 {
-printf("with proper grammar, but the outcome is a piece of art,\n")
-return (0);
+	printf("with proper grammar, but the outcome is a piece of art,\n");
+	return (0);
 }
