c8d1808c0068743aa1ce2cba5baf66ad7b4ab36b
marktos247 <aj.marktos@yahoo.com>
2023-06-17 02:31:06 -0700

    print_sizes

---
+++ 0x00-hello_world/5-printf.c
@@ -0,0 +1,10 @@
+#include <stdio>
+/**
+ * main - Print a line using printf function
+ * Return: 0
+ */
+int main(void)
+{
+printf("with proper grammar, but the outcome is a piece of art,\n")
+return (0);
+}
---
+++ 0x00-hello_world/6-size.c
@@ -0,0 +1,20 @@
+#include <stdio>
+/**
+ * main - printing the size of various computer types
+ * Return: 0
+ */
+int main(void)
+{
+	char c;
+	int i;
+	long int l;
+	long long int x;
+	float y;
+
+printf("Size of s char: %lu byte(s)\n", (unsigned long)sizeof(c));
+printf("Size of s int: %lu byte(s)\n", (unsigned long)sizeof(i));
+printf("Size of s long int: %lu byte(s)\n", (unsigned long)sizeof(l));
+printf("Size of s long long int: %lu byte(s)\n", (unsigned long)sizeof(x));
+printf("Size of s float: %lu byte(s)\n", (unsigned long)sizeof(y));
+return (0);
+}
