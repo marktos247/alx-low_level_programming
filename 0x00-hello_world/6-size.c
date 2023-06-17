14deb05b6f438b43076b41c6d2ecb25c6f003259
marktos247 <aj.marktos@yahoo.com>
2023-06-17 04:45:40 -0700

    print_sizes

--- 0x00-hello_world/6-size.c
+++ 0x00-hello_world/6-size.c
@@ -10,12 +10,10 @@ int main(void)
 	long int c;
 	long long int d;
 	float f;
-
 printf("Size of s char: %lu byte(s)\n", (unsigned long)sizeof(a));
 printf("Size of s int: %lu byte(s)\n", (unsigned long)sizeof(b));
 printf("Size of s long int: %lu byte(s)\n", (unsigned long)sizeof(c));
 printf("Size of s long long int: %lu byte(s)\n", (unsigned long)sizeof(d));
 printf("Size of s float: %lu byte(s)\n", (unsigned long)sizeof(f));
-return: (0);
+return (0);
 }
-
