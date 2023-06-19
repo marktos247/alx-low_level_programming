3e405dcb75b866c130a525098dfecd434ef0bfd4
marktos247 <aj.marktos@yahoo.com>
2023-06-19 11:04:15 -0700

    variables_if_else_while

--- 0x01-variables_if_else_while/4-print_alphabt.c
+++ 0x01-variables_if_else_while/4-print_alphabt.c
@@ -3,15 +3,13 @@
  * main - A program that prints the alphabet in lowercase
  * Return: 0
  */
-
 int main(void)
 {
         char c;
-
         c = 'a';
         while
                 (c <= 'z') {
-                        if ((c) != 'q' && c != 'c' && c <= 'z')
+			if ((c) != 'q' && c != 'c' && c <= 'z')
 				putchar(c);
                         c++;
                 }
