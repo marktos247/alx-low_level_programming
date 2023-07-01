284e94dba3c718587f1df38a00a1f269717c2fe0
marktos247 <aj.marktos@yahoo.com>
2023-07-01 08:22:23 -0700

    C - Functions, nested loops

--- 0x02-functions_nested_loops/8-24_hours.c
+++ 0x02-functions_nested_loops/8-24_hours.c
@@ -1,11 +1,11 @@
 #include "main.h"
 
 /**
- * jack_bauer - prints every minute of the day of Jack Bauer
+ * jack_baver - prints every minute of the day of Jack Bauer
  * starting from 00:00 to 23:59
  */
 
-void jack_bauer(void)
+void jack_baver(void)
 {
 	int i, j;
 
